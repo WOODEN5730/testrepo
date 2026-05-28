412750fa2a7044e111159250a1ed8ae2b47a2881
wooden5730 <wooden5730@msn.com>
2026-05-28 13:03:57 -0400

    Added User Product Experince TextArea

--- productSurvey/product_survey.js
+++ productSurvey/product_survey.js
@@ -7,6 +7,7 @@ function submitFeedback() {
     const designation = document.getElementById('designation').value;
     const productType = document.getElementById('productType').value;
     const feedback = document.getElementById('feedbackText').value;
+    const productExperince = document.getElementById('userExperince').value;
   
     document.getElementById('userName').innerHTML = username;
     document.getElementById('userAge').innerHTML = age;
@@ -15,6 +16,7 @@ function submitFeedback() {
     document.getElementById('userDesignation').innerHTML = designation;
     document.getElementById('userProductChoice').innerHTML = productType;
     document.getElementById('userFeedback').innerHTML = feedback;
+    document.getElementById('userExperinceFeedback').innerHTML = productExperince;
     document.getElementById('userInfo').style.display = 'block';
     alert('Thank you for your valuable feedback');
 }
