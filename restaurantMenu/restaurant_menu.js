d7434729d5e0bd2859f04836727d1469212481d1
wooden5730 <wooden5730@msn.com>
2026-05-28 22:06:34 -0400

    Added Menu Prices

--- restaurantMenu/restaurant_menu.js
+++ restaurantMenu/restaurant_menu.js
@@ -1,6 +1,6 @@
-const breakfastMenu = ['Pancakes', 'Eggs Benedict', 'Oatmeal', 'Frittata'];
-const mainCourseMenu = ['Steak', 'Pasta', 'Burger', 'Salmon'];
-const dessertMenu = ['Cake', 'Ice Cream', 'Pudding', 'Fruit Salad'];
+const breakfastMenu = ['Pancakes -$12', 'Eggs Benedict -$22.99', 'Oatmeal -$21.99', 'Frittata -$15.00'];
+const mainCourseMenu = ['Steak -$45.00', 'Pasta -$25.00', 'Burger -$20.00', 'Salmon -$32.50'];
+const dessertMenu = ['Cake -$9.99', 'Ice Cream -$8.99', 'Pudding -$10.99', 'Fruit Salad -$12.99'];
 
 const breakfastMenuItemsHTML = breakfastMenu.map((item, index) => `<p>Item ${index + 1}: ${item}</p>`).join('');
 document.getElementById('breakfastMenuItems').innerHTML = breakfastMenuItemsHTML;
